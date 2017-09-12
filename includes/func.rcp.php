297df2432ea89b12a2abd74b3c01fe366cbb33ea
Grégoire Noyelle <hello@gregoirenoyelle.com>
2017-09-12 11:05:26 +0200

    Ajout du champ complet pour la civilité

--- includes/func.rcp.php
+++ includes/func.rcp.php
@@ -7,9 +7,19 @@
  */
 function wpparis_rcp_add_user_fields() {
 
+	$civilite = get_user_meta( get_current_user_id(), 'rcp_civilite', true );
 	$profession = get_user_meta( get_current_user_id(), 'rcp_profession', true );
 	$ville  = get_user_meta( get_current_user_id(), 'rcp_ville', true );
+
 	?>
+    <p>
+        <label for="rcp_civilite"><?php _e( 'Civilité?', 'rcp' ); ?></label>
+        <select id="rcp_civilite" name="rcp_civilite">
+            <option value="homme" <?php selected( $civilite, 'homme'); ?>><?php _e( 'Homme', 'rcp' ); ?></option>
+            <option value="femme" <?php selected( $civilite, 'femme'); ?>><?php _e( 'Femme', 'rcp' ); ?></option>
+            <option value="autre" <?php selected( $civilite, 'autre'); ?>><?php _e( 'Autre', 'rcp' ); ?></option>
+        </select>
+    </p>
 	<p>
 		<label for="rcp_profession"><?php _e( 'Votre profession', 'rcp' ); ?></label>
 		<input name="rcp_profession" id="rcp_profession" type="text" value="<?php echo esc_attr( $profession ); ?>"/>
@@ -30,9 +40,23 @@ add_action( 'rcp_profile_editor_after', 'wpparis_rcp_add_user_fields' );
  */
 function wpparis_rcp_add_member_edit_fields( $user_id = 0 ) {
 
+	$civilite = get_user_meta( get_current_user_id(), 'rcp_civilite', true );
 	$profession = get_user_meta( $user_id, 'rcp_profession', true );
 	$ville  = get_user_meta( $user_id, 'rcp_ville', true );
 	?>
+
+    <tr valign="top">
+        <th scope="row" valign="top">
+            <label for="rcp_civilite"><?php _e( 'Civilité', 'rcp' ); ?></label>
+        </th>
+        <td>
+	        <select id="rcp_civilite" name="rcp_civilite">
+	            <option value="homme" <?php selected( $civilite, 'homme'); ?>><?php _e( 'Homme', 'rcp' ); ?></option>
+	            <option value="femme" <?php selected( $civilite, 'femme'); ?>><?php _e( 'Femme', 'rcp' ); ?></option>
+	            <option value="autre" <?php selected( $civilite, 'autre'); ?>><?php _e( 'Autre', 'rcp' ); ?></option>
+	        </select>
+        </td>
+    </tr>
 	<tr valign="top">
 		<th scope="row" valign="top">
 			<label for="rcp_profession"><?php _e( 'Profession', 'rcp' ); ?></label>
@@ -61,6 +85,9 @@ add_action( 'rcp_edit_member_after', 'wpparis_rcp_add_member_edit_fields' );
  *
  */
 function pw_rcp_save_user_fields_on_register( $posted, $user_id ) {
+	if( ! empty( $posted['rcp_civilite'] ) ) {
+		update_user_meta( $user_id, 'rcp_civilite', sanitize_text_field( $posted['rcp_civilite'] ) );
+	}
 	if( ! empty( $posted['rcp_profession'] ) ) {
 		update_user_meta( $user_id, 'rcp_profession', sanitize_text_field( $posted['rcp_profession'] ) );
 	}
@@ -76,6 +103,14 @@ add_action( 'rcp_form_processing', 'wpparis_rcp_save_user_fields_on_register', 1
  *
  */
 function wpparis_rcp_save_user_fields_on_profile_save( $user_id ) {
+	$civilite_choice = array(
+		'homme',
+		'femme',
+		'autre'
+	);
+    if( isset( $_POST['rcp_civilite'] ) && in_array( $_POST['rcp_civilite'], $civilite_choice ) ) {
+        update_user_meta( $user_id, 'rcp_civilite', sanitize_text_field( $_POST['rcp_civilite'] ) );
+    }
 	if( ! empty( $_POST['rcp_profession'] ) ) {
 		update_user_meta( $user_id, 'rcp_profession', sanitize_text_field( $_POST['rcp_profession'] ) );
 	}
